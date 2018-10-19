e0bd3b641ec3266bc6cf65cdd055585ab2fdc16d
Ryan Nguyen <shiga2714@gmail.com>
2018-10-19 19:17:55 -0400

    Shield Levels adjusted
    - Shield can now decrement its level and the ship can now be destroyed

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -14,7 +14,12 @@ public class Hero : MonoBehaviour
     public float pitchMult = 30;
 
     [Header("Set Dynamically")]
-    public float shieldLevel = 1;
+
+    [SerializeField]
+    private float _shieldLevel = 1;
+
+    // This variable holds a reference to the last triggering GameObject 
+    private GameObject lastTriggerGo = null;
 
     void Awake()
     {
@@ -39,5 +44,44 @@ public class Hero : MonoBehaviour
         transform.position = pos;
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        //print("Triggered: " + go.name);
+        if(go == lastTriggerGo)
+        {
+            return;
+        }
+        lastTriggerGo = go;
+
+        if(go.tag == "Enemy")
+        {
+            shieldLevel--; // Decrement shield level
+            Destroy(go); // Destroy enemy
+        }
+        else
+        {
+            print("Triggered by non-Enemy: " + go.name);
+        }
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            // if shield < 0
+            if(value < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
