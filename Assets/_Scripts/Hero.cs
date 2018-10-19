13f11ccce25a1619c95b70b1f41675f89eff76f7
Ryan Nguyen <shiga2714@gmail.com>
2018-10-19 16:45:07 -0400

    Hero shield created
    - Added Hero Shield
    - Wrote Shield.cs
    - Made adjustments to Shield attributes

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hello.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+    }
 }
